c98862289e27571e7a3022fc25762eba8bafa966
awalhossain <waqin21@gmail.com>
2024-03-11 00:34:38 +0600

    Add user routes to index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { UserRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/user',
+    route: UserRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
