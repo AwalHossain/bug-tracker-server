b6ffe07351d3f0214f7f36ad165781e59f5ef6ed
awalhossain <waqin21@gmail.com>
2024-03-11 00:32:17 +0600

    Refactor server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,12 @@ import app from './app';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
 
-
 async function bootstrap() {
-
   const server: Server = app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
-
     if (server) {
       server.close(() => {
         logger.info('Server closed');
