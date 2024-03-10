8d5a41290728e0f4abb5d738fed747f834f3a7f9
awalhossain <waqin21@gmail.com>
2024-03-11 00:34:31 +0600

    Add user validation schemas

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+const loginRegistrationZodSchema = z.object({
+  body: z.object({
+    email: z.string({
+      required_error: 'Email is required',
+    }),
+    password: z.string({
+      required_error: 'Password is required',
+    }),
+  }),
+});
+
+const refreshTokenZodSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh Token is required',
+    }),
+  }),
+});
+
+export const UserValidation = {
+  loginRegistrationZodSchema,
+  refreshTokenZodSchema,
+};
