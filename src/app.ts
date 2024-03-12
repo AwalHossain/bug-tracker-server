f7b3016caf4f9010c1916d24520a75a147b4ab47
awalhossain <waqin21@gmail.com>
2024-03-12 23:18:33 +0600

    Add CORS configuration for localhost

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 //parser
