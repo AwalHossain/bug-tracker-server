44ed7e7e84a13cf9cc6b695fdc280eff864a0044
awalhossain <waqin21@gmail.com>
2024-03-13 03:56:30 +0600

    Update server.ts logging statements

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,23 @@
 import { Server } from 'http';
 import app from './app';
 import config from './config';
-import { errorlogger, logger } from './shared/logger';
 
 async function bootstrap() {
   const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
+    console.log(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
     if (server) {
       server.close(() => {
-        logger.info('Server closed');
+        console.log('Server closed');
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
+    console.log(error);
     exitHandler();
   };
 
@@ -26,7 +25,7 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
+    console.log('SIGTERM received');
     if (server) {
       server.close();
     }
