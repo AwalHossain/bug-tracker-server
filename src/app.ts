9ece0f7f2f54a94c4cef69632d5d093f4f1db6ba
awalhossain <waqin21@gmail.com>
2024-03-13 03:52:47 +0600

    Update CORS origin to use config.client_url

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,13 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
 
 import cookieParser from 'cookie-parser';
+import config from './config';
 
 const app: Application = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: config.client_url || 'http://localhost:3000',
     credentials: true,
   })
 );
