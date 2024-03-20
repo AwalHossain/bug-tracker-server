4938a38daf08c692466f69249d05466bb09e31e0
awalhossain <waqin21@gmail.com>
2024-03-21 00:28:40 +0600

    Add invitation configuration to index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,4 +16,8 @@ export default {
     expires_in: process.env.JWT_EXPIRES_IN,
     refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
   },
+  invitation: {
+    expires_in: process.env.INVITATION_EXPIRES_IN,
+    secret: process.env.INVITATION_SECRET,
+  },
 };
