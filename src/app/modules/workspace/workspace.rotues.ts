13428b5092ae211934455ff2bc37ba93be0b3cba
awalhossain <waqin21@gmail.com>
2024-03-21 12:52:40 +0600

    Refactor workspace routes to include authentication and authorization

--- src/app/modules/workspace/workspace.rotues.ts
+++ src/app/modules/workspace/workspace.rotues.ts
@@ -1,8 +1,38 @@
 import { Router } from 'express';
-import { InvitationController } from './workspace.controller';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import { WorkspaceController } from './workspace.controller';
 
 const router = Router();
 
-router.post('/create', InvitationController.createInvitation);
+router.post(
+  '/create',
+  auth(ENUM_USER_ROLE.ADMIN),
+  WorkspaceController.createWorkspace
+);
+
+router.get(
+  '/get-all',
+  auth(ENUM_USER_ROLE.ADMIN),
+  WorkspaceController.getAllWorkspace
+);
+
+router.get(
+  '/get-one/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  WorkspaceController.getOneWorkspace
+);
+
+router.put(
+  '/update/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  WorkspaceController.updateWorkspace
+);
+
+router.delete(
+  '/delete/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  WorkspaceController.deleteWorkspace
+);
 
 export const WorkspaceRoutes = router;
