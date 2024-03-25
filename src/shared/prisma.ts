4554637059c250c600761eb067b16f2480ac5f97
awalhossain <waqin21@gmail.com>
2024-03-26 00:12:01 +0600

    Refactor Prisma client initialization and add global singleton

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,6 +1,22 @@
+/* eslint-disable no-var */
+/* eslint-disable no-undef */
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+// const prisma = new PrismaClient().$extends(withAccelerate());
+
+// export default prisma;
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
