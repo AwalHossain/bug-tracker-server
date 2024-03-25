163c14e1ae3a6e01df586e652afcd9a7d194659f
awalhossain <waqin21@gmail.com>
2024-03-26 00:11:55 +0600

    Add console logs for password and isMatch in comparePassword function

--- src/utils/comaprePassword.ts
+++ src/utils/comaprePassword.ts
@@ -6,7 +6,11 @@ export const comparePassword = async (
   password: string,
   hashedPassword: string
 ) => {
+  console.log(password, 'password in comparePassword', hashedPassword);
+
   const isMatch = await bcrypt.compare(password, hashedPassword);
+  console.log(isMatch, 'isMatch in comparePassword');
+
   if (!isMatch) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid email or password');
   }
